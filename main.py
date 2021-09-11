a40c717670e0af70ce127b2ef736aaf661580977
gomonuk <gomoniukde@biocad.ru>
2021-09-11 23:47:31 +0300

    g2

--- main.py
+++ main.py
@@ -1,86 +1,33 @@
-import logging
-from queue import Queue
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-import cherrypy
-import telegram
-from telegram.ext import CommandHandler, MessageHandler, Filters, Dispatcher
-
-from settings import NAME, PORT, TOKEN, CHAT_ID
-
-
-class SimpleWebsite:
-    @cherrypy.expose
-    def index(self):
-        return """<H1>Welcome!</H1>"""
+TOKEN = "1970454158:AAGPh2dEwz1Z18p6EoPDQnyZlGRwTN1l5l8"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-class BotComm:
-    exposed = True
+def check(in_str: str) -> bool:
+    return bool(in_str)
 
-    def __init__(self, TOKEN, NAME):
-        super(BotComm, self).__init__()
-        self.TOKEN = TOKEN
-        self.NAME = NAME
-        self.bot = telegram.Bot(self.TOKEN)
-        try:
-            self.bot.setWebhook(
-                "https://{}.herokuapp.com/{}".format(self.NAME, self.TOKEN))
-        except:
-            raise RuntimeError("Failed to set the webhook")
 
-        self.update_queue = Queue()
-        self.dp = Dispatcher(self.bot, self.update_queue)
+def calc(string: str) -> int:
+    return eval(string)
 
-        self.dp.add_handler(CommandHandler("start", self._start))
-        self.dp.add_handler(MessageHandler(Filters.text, self._process_update))
-        self.dp.add_error_handler(self._error)
 
-    @cherrypy.tools.json_in()
-    def POST(self, *args, **kwargs):
-        update = cherrypy.request.json
-        update = telegram.Update.de_json(update, self.bot)
-        self.dp.process_update(update)
+@dp.message_handler(commands=["start", "help"])
+async def send_welcome(msg: types.Message):
+    await msg.reply(f"Приятно познакомиться,{msg.from_user.first_name}")
 
-    def _error(self, error):
-        cherrypy.log("Error occurred - {}".format(error))
 
-    def _start(self, bot, update):
-        update.effective_message.reply_text('Милые девушки. Этот бот создан специально для вас. Сделайте заказ, например, "латте без сахара" или "черный чай с одним кусочком сахара и печенькой". А мы обеспечим оперативную доставку вашего желания прямо на ваше рабочее место')
+@dp.message_handler(content_types=["text"])
+async def get_text_messages(msg: types.Message):
+    answer = "Не работает"
+    if check(msg.text.lower()):
+        answer = calc(msg.text.lower())
 
-    def _accept_order(self, bot, update):
-        order_text = update.effective_message.text
-        order_user = update.effective_message.from_user
-        order_user_first_name = order_user.first_name
-        order_user_last_name = order_user.last_name
-        order_user_username = order_user.username
-        text = "{first_name} {last_name} ({username}) " \
-               "желает: {order}".format(first_name=order_user_first_name,
-                                        last_name=order_user_last_name,
-                                        username=order_user_username,
-                                        order=order_text)
-        self.bot.send_message(chat_id=CHAT_ID, text=text)
-        update.effective_message.reply_text("Ваш заказ принят!")
-
-    def _process_update(self, bot, update):
-        chat_id = update.effective_message.chat.id
-        if not chat_id == CHAT_ID:
-            self._accept_order(bot, update)
+    await msg.answer(answer)
 
 
 if __name__ == "__main__":
-    # Enable logging
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO)
-    logger = logging.getLogger(__name__)
-
-    # Set up the cherrypy configuration
-    cherrypy.config.update({"server.socket_host": "0.0.0.0", })
-    cherrypy.config.update({"server.socket_port": int(PORT), })
-    cherrypy.tree.mount(SimpleWebsite(), "/")
-    cherrypy.tree.mount(
-        BotComm(TOKEN, NAME),
-        "/{}".format(TOKEN),
-        {"/": {
-            "request.dispatch": cherrypy.dispatch.MethodDispatcher()}})
-    cherrypy.engine.start()
+    executor.start_polling(dp)
